cd43eaf67cd7573d64be569ce59e369a6f1fecc8
JingLiu122 <jingfengliu231@gmail.com>
2020-05-04 09:51:51 -0400

    track files...

---
+++ src/components/DisplayField.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function displayField(props) {
+  const { dictionary, onDeleteItem } = props;
+  
+  function handleDelete(e) {
+    onDeleteItem(e.target.name);
+  }
+
+  const listItems = dictionary.map((item) => 
+    <li key=
+      {item.vocabulary}>{`${item.vocabulary}: ${item.type} | ${item.definition}`}
+      <button name={item.vocabulary} onClick={handleDelete}>Delete</button>
+    </li>
+  );
+
+  return (
+    <div>
+      Vocabularies
+      <ul>{listItems}</ul>
+    </div>
+  );
+}
+
+export default displayField;
