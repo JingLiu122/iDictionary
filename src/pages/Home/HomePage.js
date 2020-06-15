904896875a5f8059fd2a36f8bb175f4fcd668b8e
JingLiu122 <jingfengliu231@gmail.com>
2020-06-15 11:44:06 -0400

    going to implement the front end with material-ui framework; create its branch first

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,66 +1,111 @@
-import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
-import { store } from '../../redux';
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { store } from "../../redux";
 
 // import { AddVocabulary, DisplayField, SearchField, ResultField } from './features';
-import AddVocabulary from './features/AddVocabulary';
-import DisplayField from './features/DisplayField';
-import SearchField from './features/SearchField';
-import ResultField from './features/ResultField';
+import AddVocabulary from "./features/AddVocabulary";
+import DisplayField from "./features/DisplayField";
+import SearchField from "./features/SearchField";
+import ResultField from "./features/ResultField";
 
-function HomePage({dictionary}) {
-  // const initialDictionary = () => JSON.parse(localStorage.getItem('dictionary')) || [{vocabulary: "futz", type: "verb", definition: "waste time; idle or busy oneself aimlessly"}];
-  // declare local state variables
-  const [d, setDictionary] = useState(dictionary);
-  const [search, setSearch] = useState('');
+import { Header, Sidebar, Dashboard } from "../../components";
 
-  // side-effect hooks
-  useEffect(() => {
-    // obj.toString() => [object Object]
-    // localStorage.setItem("dictionary", JSON.stringify(d));
-    store.subscribe(()=>{
-      localStorage.setItem('dictionary', JSON.stringify(store.getState()))
-    })
-  }, [d]);
+import { Grid, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-  function handleInputItem(dict) {
-    let index = d.findIndex(item => item.vocabulary === dict.vocabulary);
-    const word = {vocabulary: dict.vocabulary, type: dict.type, definition: dict.definition};
-    if(index > -1) {
-      const list = [...d];
-      list[index] = word
-      setDictionary(list);
-    } else {
-      setDictionary([...d, word]);  
-    }
-  }
+const useStyles = makeStyles({
+	root: {
+		display: "flex",
+		whiteSpace: "nowrap",
+	},
+	spacing: {
+		marginTop: "12%",
+		marginBottom: 10,
+	},
+});
 
-  const handleDeleteItem = (value) => {
-    const filteredDictionary = d.filter((item) => item.vocabulary !== value);
-    setDictionary(filteredDictionary);
-  }
+function HomePage({ dictionary }) {
+	// const initialDictionary = () => JSON.parse(localStorage.getItem('dictionary')) || [{vocabulary: "futz", type: "verb", definition: "waste time; idle or busy oneself aimlessly"}];
+	// declare local state variables
+	const [d, setDictionary] = useState(dictionary);
+	const [search, setSearch] = useState("");
 
-  const handleSearchItem = (word) => {
-    setSearch(word);
-  }
+	const classes = useStyles();
 
-  return (
-    <div className="App">
-      <header className="App-header">          
-        <AddVocabulary onInputItem={handleInputItem}/>
-        <br />
-        <DisplayField onDeleteItem={handleDeleteItem}/>
+	// side-effect hooks
+	useEffect(() => {
+		// obj.toString() => [object Object]
+		// localStorage.setItem("dictionary", JSON.stringify(d));
+		store.subscribe(() => {
+			localStorage.setItem("dictionary", JSON.stringify(store.getState()));
+		});
+	}, [d]);
 
-        <SearchField onSearchItem={handleSearchItem} />
-        <ResultField />
-      </header>
-    </div>
-  );
+	function handleInputItem(dict) {
+		let index = d.findIndex((item) => item.vocabulary === dict.vocabulary);
+		const word = {
+			vocabulary: dict.vocabulary,
+			type: dict.type,
+			definition: dict.definition,
+		};
+		if (index > -1) {
+			const list = [...d];
+			list[index] = word;
+			setDictionary(list);
+		} else {
+			setDictionary([...d, word]);
+		}
+	}
+
+	const handleDeleteItem = (value) => {
+		const filteredDictionary = d.filter((item) => item.vocabulary !== value);
+		setDictionary(filteredDictionary);
+	};
+
+	const handleSearchItem = (word) => {
+		setSearch(word);
+	};
+
+	// material-ui
+	const [open, setOpen] = useState(false);
+
+	const setDrawerButton = (value) => {
+		setOpen(value);
+	};
+
+	return (
+		<div className={classes.root}>
+			<Header open={open} setDrawerButton={setDrawerButton} />
+			<Sidebar open={open} setDrawerButton={setDrawerButton} />
+			<Dashboard />
+			{/* <Grid container>
+				<Grid item xs="auto" sm="auto" className={classes.spacing}>
+					<Paper>
+						<AddVocabulary onInputItem={handleInputItem} />
+						<br />
+						<DisplayField onDeleteItem={handleDeleteItem} />
+
+						<SearchField onSearchItem={handleSearchItem} />
+						<ResultField />
+					</Paper>
+				</Grid> */}
+			{/* <div className="App">
+				<header className="App-header">
+					<AddVocabulary onInputItem={handleInputItem} />
+					<br />
+					<DisplayField onDeleteItem={handleDeleteItem} />
+
+					<SearchField onSearchItem={handleSearchItem} />
+					<ResultField />
+				</header>
+			</div> */}
+		</div>
+	);
 }
 
-const mapStateToProps = state => {
-  const { dictionary } = state;
-  return { dictionary };
-}
+const mapStateToProps = (state) => {
+	const { dictionary } = state;
+	return { dictionary };
+};
 
 export default connect(mapStateToProps)(HomePage);
